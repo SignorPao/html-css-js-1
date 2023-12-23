0c87c57591c26046df480f075e48a994b7add9b0
Pavel Belousov <signorpao@gmail.com>
2023-12-23 10:46:59 +0300

    done.

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,5 +27,30 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SWIPER WACTHES ===============*/
+const swiperWatches = new Swiper(".home__swiper", {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      translate: [-100, 0, -500],
+      rotate: [0, 0, 15],
+      opacity: 0,
+    },
+    next: {
+      translate: [100, 0, -500],
+      rotate: [0, 0, -15],
+      opacity: 0,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 /*=============== GSAP ANIMATION ===============*/
+gsap.from(".home__images", 1.5, { opacity: 0, y: 150, delay: 0.1 });
+gsap.from(".home__data", 1.8, { opacity: 0, x: -100, delay: 0.8 });
+gsap.from(".home__info", 1.8, { opacity: 0, x: -100, delay: 1 });
